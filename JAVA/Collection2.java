7f2a1eae6f7ed1a062859bb63f4ea9f2340cd830
Ashiq-ma <112387625+Ashiq-ma@users.noreply.github.com>
2022-09-07 10:56:06 +0530

    Add files via upload

---
+++ JAVA/Collection2.java
@@ -0,0 +1,14 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Collection2 {
+public static void main(String args[]) {
+	List<Integer> lst=new ArrayList<Integer>();
+	lst.add(456);
+	lst.add(123);
+	lst.add(789);
+	lst.add(134);
+}
+}
---
+++ JAVA/Map1.java
@@ -0,0 +1,14 @@
+package collections;
+
+import java.util.HashMap;
+
+public class Map1 {
+	public static void main(String args[]) {
+		HashMap<Integer,String>m= new HashMap<Integer,String>();
+		m.put(1, "glory");
+		m.put(null, null);
+		m.put(1, "haridev");
+		System.out.println(m.get(2));
+	}
+
+}
