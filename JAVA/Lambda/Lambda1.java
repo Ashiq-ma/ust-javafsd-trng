75e75e3293ed30eac30655393ea749895fdab4b9
Ashiq-ma <112387625+Ashiq-ma@users.noreply.github.com>
2022-09-20 14:43:16 +0530

    Add files via upload

---
+++ JAVA/Lambda/Lambda1.java
@@ -0,0 +1,18 @@
+package lambda.examples;
+
+
+@FunctionalInterface
+interface MyInterface{
+	double getPiValue();
+}
+public class Lambda1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyInterface ref;
+		ref=()->3.1415;
+		System.out.println("Value of pi="+ref.getPiValue());
+
+	}
+
+}
---
+++ JAVA/Lambda/Lambda5.java
@@ -0,0 +1,20 @@
+package lambda.examples;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lambda5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> list=new ArrayList<String>();
+		list.add("ankit");
+		list.add("mayank");
+		list.add("irfan");
+		list.add("jai");
+		list.forEach((n)->System.out.println(n));
+		
+
+	}
+
+}
