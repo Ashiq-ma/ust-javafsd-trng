ac33ccf14c6fdd812ef64fedfee9f3dbd1753acd
ustjavafsdb214 <235714@ust.com>
2022-10-31 17:47:54 +0530

    Changed

--- JAVA/Array.java
+++ JAVA/Array.java
@@ -1,6 +1,14 @@
 package loops;
 
+/**
+ * The type Array.
+ */
 public class Array {
+	/**
+	 * Main.
+	 *
+	 * @param args the args
+	 */
 	public static void main(String args[]) {
 		int[] numbers= {3,7,5,-5};
 		for(int number:numbers) {
